dc3d841cdb978b37fe3da2bcc2a3db7fcbe414ea
Darren Britton <darrenbritton@hotmail.com>
2015-10-27 00:39:58 +0000

    structured and created files for docker compose

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM php:5.6-apache
+ADD app /var/www/html/
+EXPOSE 80
---
+++ app/config/db.php
@@ -0,0 +1,6 @@
+<?php
+define("DB_HOST", getenv("DB_PORT_3306_TCP_ADDR"));
+define("DB_NAME", getenv("DB_ENV_MYSQL_DATABASE"));
+define("DB_USER", getenv("DB_ENV_MYSQL_USER"));
+define("DB_PASS", getenv("DB_ENV_MYSQL_PASSWORD"));
+?>
